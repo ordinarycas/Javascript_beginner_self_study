456c33e66fbb78fd6ff7212b3d86dce48d665d85
xcqvmywoj <10210215@mail.chihlee.edu.tw>
2017-05-09 02:59:02 +0800

    6.11 break
    識別字（identifier），標籤（label）

--- LearnJS04.js
+++ LearnJS04.js
@@ -1,13 +1,50 @@
-test(1);
+//switch 舉例
+console.log(convert("123"));
 
-function test(o){
-	var i = 0;
-	if(typeof o == "object"){
-		var j = 0;
-		for(var k = 0 ; k < 10 ; k++){
-			console.log(k);
-		}
-		console.log(k);
+function convert(x){
+	switch(typeof x){
+		case 'number':
+			return x.toString(16);
+		case 'string':
+			return '"' + x + '"';
+		case 'boolean':
+			return x.toString().toUpperCase();
+		default:
+			return x.toString();
 	}
-	console.log(j);
 }
+
+var person = {fname:"John", lname:"Doe", age:25}; 
+var text = "";
+var x;
+for (x in person) {
+    text += person[x]+" ";
+}
+console.log(text);
+
+//識別字（identifier），標籤（label）
+var cars = ["BMW", "Volvo", "Saab", "Ford"];
+var text = "";
+list: {
+    text += cars[0] + "  "; 
+    text += cars[1] + "  "; 
+    text += cars[2] + "  "; 
+    break list; //跳出此標籤
+    text += cars[3] + "  "; 
+    text += cars[4] + "  "; 
+    text += cars[5] + "  "; 
+}
+console.log(text);
+
+//範例6.11
+outerloop:
+	for (var i = 0; i < 10; i++) {
+		innerloop:
+		for (var j = 0; j < 10; j++) {
+			if (j > 3) break;
+			if (i == 2) break innerloop;
+			if (i == 4) break outerloop;
+			console.log("i = "+i+"，j = " + j);
+		}
+	}
+console.log("FINAL i = "+i+"，j = " + j);
